dc566bf3e3e82b2f23b35788ed5c24ab36fbc23b
La-wii <sentilavale@gmail.com>
2021-06-01 14:58:32 +0200

    stampa parola + corta per prima

--- es-1/js/script.js
+++ es-1/js/script.js
@@ -8,13 +8,10 @@ var lunghezzaParolaDue = parolaDue.length;
 // Il software stampa prima la parola più corta, poi la parola più lunga.
 
 if (lunghezzaParolaUno == lunghezzaParolaDue) {
-    console.log("uguali");
     document.getElementById('uno').innerHTML = 'Le due parole sono uguali' + ' ' + parolaUno + ' ' + parolaDue;
-} else if (lunghezzaParolaUno > lunghezzaParolaDue) {
-    console.log("parola uno più lunga");
-    document.getElementById('uno').innerHTML = 'La parola più lunga è :' + ' ' + parolaUno; 
+} else if (lunghezzaParolaUno < lunghezzaParolaDue) {
+    document.getElementById('uno').innerHTML = 'La parola più lunga è :' + ' ' + parolaUno + ' ' + parolaDue; 
 } else {
-    console.log("parola due più lunga");
-    document.getElementById('uno').innerHTML = 'La parola più lunga è :' + ' ' + parolaDue; 
+    document.getElementById('uno').innerHTML = 'La parola più lunga è :' + ' ' + parolaDue + ' ' + parolaUno; 
 }
 
