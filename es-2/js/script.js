0f10dabe7afd04ba38f4ce03cd45bcacf270c796
La-wii <sentilavale@gmail.com>
2021-06-01 15:30:51 +0200

    es.2 ciclo for

---
+++ es-2/js/script.js
@@ -0,0 +1,22 @@
+// Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri   inseriti. 
+// Esegui questo programma in due versioni, con il for 
+
+var somma = 0;
+
+for (var i = 0; i < 5; i++){
+    numero = parseInt(prompt('inserisci un numero'));
+    somma += numero;   
+}
+document.getElementById('somma').innerHTML = somma;
+console.log(somma);
+
+// NOTE: ALTRO METODO SOMMA:    somma = somma + numero;
+
+// Con il while.
+// var i = 0; 
+
+// while (i < 5){
+//     numero = parseInt(prompt('inserisci un numero'));
+//     somma += numero; 
+//     i++;
+// }
